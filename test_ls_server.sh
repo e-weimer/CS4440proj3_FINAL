f8c20bb3199616514f2a2fd3b76cfd20f1adc473
Emily Weimer <147999373+e-weimer@users.noreply.github.com>
2025-11-23 05:11:18 +0000

    Updated file names in test_ls_server.sh

--- test_ls_server.sh
+++ test_ls_server.sh
@@ -5,7 +5,7 @@ PORT=5560
 
 echo "Compiling ls_server and ls_client..."
 gcc -Wall -Wextra -o ls_server ls_server.c
-gcc -Wall -Wextra -o ls_client ls_client_v2.c
+gcc -Wall -Wextra -o ls_client ls_client.c
 
 echo
 echo "========== LS SERVER NEGATIVE TESTS =========="
